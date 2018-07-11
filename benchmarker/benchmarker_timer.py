4d546ca38769302b9d98a018520210c1936c2b2a
Joel  Southgate <joelsouthgate@googlemail.com>
2018-07-11 08:03:06 +0000

    benchmarker_timer.py edited online with Bitbucket

--- benchmarker/benchmarker_timer.py
+++ benchmarker/benchmarker_timer.py
@@ -8,17 +8,18 @@ def time_commands(filename, curwd):
         """Take the name of a file containing commands
            and a working directory, call subrocess
            with /usr/bin/time for each command in the file"""
-        for command in open(curwd+"/"+filename):
-                if command.startswith("#"):
-                         #in this case, we don't want to time it
-                         print("not timing: ", command[1:])
-                         subprocess.call(command[1:].strip(), shell=True, cwd=curwd)
-                else:
-                         #t_command and date_time are full command and
-                         #current date and time, respectively
-                         t_command = "/usr/bin/time -v -a -o ../output/command_times.out -p sh -c " +"'"+ command.strip()+"'"
-                         print(t_command)
-                         date_time = """echo '\tSubmission date and time: ' $(date "+%Y-%m-%d %H:%M:%S")'\n' >> ../output/command_times.out"""
-                         subprocess.call(t_command, shell=True, cwd=curwd)
-                         subprocess.call(date_time, shell=True, cwd=curwd)
+        with open(curwd+"/"+filename) as f:
+                for command in f:
+                        if command.startswith("#"):
+                                 #in this case, we don't want to time it
+                                 print("not timing: ", command[1:])
+                                 subprocess.call(command[1:].strip(), shell=True, cwd=curwd)
+                        else:
+                                 #t_command and date_time are full command and
+                                 #current date and time, respectively
+                                 t_command = "/usr/bin/time -v -a -o ../output/command_times.out -p sh -c " +"'"+ command.strip()+"'"
+                                 print(t_command)
+                                 date_time = """echo '\tSubmission date and time: ' $(date "+%Y-%m-%d %H:%M:%S")'\n' >> ../output/command_times.out"""
+                                 subprocess.call(t_command, shell=True, cwd=curwd)
+                                 subprocess.call(date_time, shell=True, cwd=curwd)
         return 0
